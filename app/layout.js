f904d61445a753581ca7b9f07a08daa5b0ce9b62
umarwaseemcowlar <umar.waseem@cowlar.com>
2024-07-02 20:49:51 +0500

    meta tags for social link preview

--- app/layout.js
+++ app/layout.js
@@ -12,10 +12,18 @@ export const metadata = {
   description: "pakistan based full stack software engineer and computer science grad",
 };
 
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta property="og:title" content="muhammad umar waseem" />
+        <meta property="og:site_name" content="muhammad umar waseem" />
+        <meta property="og:url" content="https://muhammadumarwaseem.vercel.app" />
+        <meta property="og:description" content="pakistan based full stack software engineer and computer science gradudate" />
+        <meta property="og:type" content="profile" />
+        <meta property="og:image" content="https://avatars.githubusercontent.com/u/76011160?v=4"></meta>
       </head>
       <body className={`${inter.className}`}>
         <Navbar />
