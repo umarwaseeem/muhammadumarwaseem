cc157dfc47c457a8d61eb062fc2214e1aef5b6c0
umarwaseemcowlar <umar.waseem@cowlar.com>
2024-07-02 15:46:07 +0500

    refactor: Remove unused configuration files and code

---
+++ components/ui/navbar.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav className="bg-black pt-8">
+            <ul className="flex flex-row space-x-8 justify-center">
+                <li>
+                    <Link href="/" className="text-white text-lg lg:text-2xl hover:text-gray-400">home</Link>
+                </li>
+                <li>
+                    <Link href="/about" className="text-white text-lg lg:text-2xl hover:text-gray-400">about</Link>
+                </li>
+                <li>
+                    <Link href="/projects" className="text-white text-lg lg:text-2xl hover:text-gray-400">projects</Link>
+                </li>
+                <li>
+                    <Link href="/blogs" className="text-white text-lg lg:text-2xl hover:text-gray-400">blogs</Link>
+                </li>
+                <li>
+                    <Link href="/contact" className="text-white text-lg lg:text-2xl hover:text-gray-400">contact</Link>
+                </li>
+
+            </ul>
+        </nav >
+    );
+}
