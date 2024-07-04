709b5b177fafd52b936ba29be0d01fa833d3d793
umarwaseemcowlar <umar.waseem@cowlar.com>
2024-07-04 20:44:51 +0500

    chore: projects route created

---
+++ app/projects/page.js
@@ -0,0 +1,12 @@
+import PageWrapper from "../components/pagewrapper";
+
+export default function About() {
+    return (
+        <PageWrapper>
+            <section className="flex h-[93vh] flex-col items-center lg:items-start lg:justify-start bg-midnightblue p-16">
+                <h2 className="font-bold text-4xl lg:text-8xl text-white mb-14">projects</h2>
+            </section>
+        </PageWrapper>
+
+    );
+}
