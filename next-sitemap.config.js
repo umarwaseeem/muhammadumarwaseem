578bcff099ebc3e8b5eaf44da690d3bbc46e260b
Muhammad Umar Waseem <umar.waseem@gmail.com>
2024-07-09 19:04:16 +0500

    chore: sitemap and robots.txt for improved SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const root = "https://muhammadumarwaseem.com";
+
+// Read mdx blog files
+const files = fs.readdirSync('blogs');
+const blogSlugs = files.map((file) => {
+    return file.replace('.mdx', '');
+});
+
+module.exports = {
+    siteUrl: root,
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    changefreq: 'weekly',
+    priority: 0.8,
+    exclude: [],
+    additionalPaths: async (config) => [
+        {
+            loc: `${root}`,
+            lastmod: new Date().toISOString(),
+            changefreq: 'weekly',
+            priority: 1,
+        },
+        {
+            loc: `${root}/about/`,
+            lastmod: new Date().toISOString(),
+            changefreq: 'weekly',
+            priority: 0.8,
+        },
+        {
+            loc: `${root}/blogs/`,
+            lastmod: new Date().toISOString(),
+            changefreq: 'weekly',
+            priority: 0.8,
+        },
+        {
+            loc: `${root}/projects/`,
+            lastmod: new Date().toISOString(),
+            changefreq: 'weekly',
+            priority: 0.8,
+        },
+        {
+            loc: `${root}/contact/`,
+            lastmod: new Date().toISOString(),
+            changefreq: 'weekly',
+            priority: 0.8,
+        },
+        ...blogSlugs.map((slug) => ({
+            loc: `${root}/blogs/${slug}`,
+            lastmod: new Date().toISOString(),
+            changefreq: 'weekly',
+            priority: 0.8,
+        })),
+    ],
+};
