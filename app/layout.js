8867e1900fe3eaaf916b07aef37bd05dc4a82f55
Umar-Waseem <umar.waseem@gmail.com>
2024-07-28 23:31:06 +0300

    feat: google analytics setup

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Exo_2 } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/ui/navbar";
+import GoogleAnalytics from './components/googleanalytics';
 
 const exo2 = Exo_2({
   subsets: ["latin"],
@@ -11,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={exo2.className}>
+        <GoogleAnalytics />
         <Navbar />
         {children}
       </body>
