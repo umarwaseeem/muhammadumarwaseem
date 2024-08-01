974cbc898704a0de11889e27b7c180f1c87a4910
Umar-Waseem <umar.waseem@gmail.com>
2024-08-01 04:17:04 +0300

    blog in footer

--- components/ui/footer.jsx
+++ components/ui/footer.jsx
@@ -8,12 +8,13 @@ const caveat = Caveat({
 
 export default function Footer() {
     return (
-        <section className="bg-black h-[40vh] lg:p-16 p-4 text-white flex flex-col justify-between">
+        <section className="bg-black h-[50vh] lg:p-16 p-4 text-white flex flex-col justify-between">
             <div>
                 <Link href={"/"} className={`text-4xl font-bold text-white ${caveat.className}`}>Umar Waseem</Link>
                 <ul className='flex flex-col pl-1 pt-4 space-y-2'>
                     <l1><Link href={"/about"}>About</Link></l1>
                     <l1><Link href={"/projects"}>Projects</Link></l1>
+                    <l1><Link href={"/blogs"}>Blogs</Link></l1>
                     <l1><Link href={"/contact"}>Contact</Link></l1>
                     <l1><Link href={"/resume"}>Resume</Link></l1>
                     <l1><Link href={"/now"}>Now</Link></l1>
