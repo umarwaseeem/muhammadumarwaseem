899ce5d3094ad00f4c9611ae958c2a2fed5b98c3
Umar-Waseem <umar.waseem@gmail.com>
2024-08-01 11:07:06 +0300

    chore: add structured data

--- app/layout.js
+++ app/layout.js
@@ -9,9 +9,42 @@ const exo2 = Exo_2({
   weight: ["400"],
 });
 
+const JSONLD = ({ data }) => (
+  <script
+    type="application/ld+json"
+    dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+  />
+);
+
+const structuredData = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  "url": "https://muhammadumarwaseem.com",
+  "logo": "https://muhammadumarwaseem.com/favicon-32x32.png",
+  "contactPoint": {
+    "@type": "EmailAddress",
+    "email": "umar.waseem@example.com",
+    "contactType": "personal email"
+  },
+  "sameAs": [
+    "https://twitter.com/umarwaseeem",
+    "https://www.linkedin.com/in/umarwaseeem/",
+    "https://github.com/umarwaseeem"
+  ],
+  "name": "Muhammad Umar Waseem",
+  "image": "https://avatars.githubusercontent.com/u/76011160?v=4",
+  "description": "Muhammad is a Pakistan-based Full Stack Software Engineer and CS graduate skilled in Web, Mobile app, and AI solutions for diverse clients.",
+  "jobTitle": "Software Engineer",
+  "address": {
+    "addressCountry": "Pakistan"
+  }
+};
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <JSONLD data={structuredData} />
       <body className={exo2.className}>
         <GoogleAnalytics />
         <Navbar />
