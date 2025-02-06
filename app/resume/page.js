275657de7c16cddfe652b0a95fa019a2b091e5f8
umarwaseeem <umar.waseem@gmail.com>
2025-02-06 05:15:49 +0500

    update resume file

--- app/resume/page.js
+++ app/resume/page.js
@@ -6,7 +6,7 @@ export default function Resume() {
     return (
         <section className="p-6 bg-midnightblue text-white lg:px-[20%]">
             <div className="flex flex-col items-center pt-20">
-                <Link href="/ResumeUmarWaseem.pdf" className="text-sm lg:text-md text-red-500 font-semibold mb-6 underline underline-offset-4">Download The Resume</Link>
+                <Link href="/ResumeMuhammadUmarWaseem.pdf" className="text-sm lg:text-md text-red-500 font-semibold mb-6 underline underline-offset-4">Download The Resume</Link>
                 <ResumeComponent />
             </div>
         </section>
@@ -22,7 +22,7 @@ export async function generateMetadata() {
         openGraph: {
             title: "Resume | Muhammad Umar Waseem",
             description: "Professional resume of Muhammad Umar Waseem.",
-            url: "https://muhammadumarwaseem.com/resume",
+            url: "https://muhammadumarwaseem.com/blogs",
             images: umarAvatar,
         },
     };
